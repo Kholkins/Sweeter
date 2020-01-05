1c3a70d84ae6fa4fa32fc451e372ef17e608c1d3
Эдуард <kholkins@mail.ru>
2020-01-05 20:55:18 +0300

    Role Admin

--- src/main/java/Hello/org/example/Sweeter/controller/MainController.java
+++ src/main/java/Hello/org/example/Sweeter/controller/MainController.java
@@ -21,7 +21,6 @@ import java.util.UUID;
 
 @Controller
 public class MainController {
-
     @Autowired
     private MessageRepo messageRepo;
 
@@ -29,24 +28,26 @@ public class MainController {
     private String uploadPath;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model)
-    {
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
+
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        if (filter != null && !filter.isEmpty()){
+        if (filter != null && !filter.isEmpty()) {
             messages = messageRepo.findByTag(filter);
-        }else {
+        } else {
             messages = messageRepo.findAll();
         }
 
         model.addAttribute("messages", messages);
         model.addAttribute("filter", filter);
+
         return "main";
     }
+
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
@@ -56,24 +57,27 @@ public class MainController {
     ) throws IOException {
         Message message = new Message(text, tag, user);
 
-        if (file != null) {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
             File uploadDir = new File(uploadPath);
-            if (!uploadDir.exists()){
+
+            if (!uploadDir.exists()) {
                 uploadDir.mkdir();
             }
+
             String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile+"."+ file.getOriginalFilename();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
-            file.transferTo(new File(resultFilename));
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
 
-            message.setFilename(uploadPath+"/"+resultFilename);
+            message.setFilename(resultFilename);
         }
 
         messageRepo.save(message);
+
         Iterable<Message> messages = messageRepo.findAll();
+
         model.put("messages", messages);
+
         return "main";
     }
-
-
 }
