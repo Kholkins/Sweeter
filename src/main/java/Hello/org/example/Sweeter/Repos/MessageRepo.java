2da513c91b76e286daa246e88e7a969aa799d8fc
Эдуард <kholkins@mail.ru>
2020-01-08 19:37:58 +0300

    Bootstrap

--- src/main/java/Hello/org/example/Sweeter/Repos/MessageRepo.java
+++ src/main/java/Hello/org/example/Sweeter/Repos/MessageRepo.java
@@ -2,9 +2,11 @@ package Hello.org.example.Sweeter.Repos;
 
 import Hello.org.example.Sweeter.Domain.Message;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MessageRepo extends CrudRepository<Message, Long> {
     List<Message> findByTag(String tag);
 }
--- src/main/java/Hello/org/example/Sweeter/Repos/UserRepo.java
+++ src/main/java/Hello/org/example/Sweeter/Repos/UserRepo.java
@@ -3,7 +3,9 @@ package Hello.org.example.Sweeter.Repos;
 
 import Hello.org.example.Sweeter.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository <User, Long> {
     User findByUsername(String username);
 }
